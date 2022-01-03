f56c3a5a144ca98b0f0b13ef38bbe4b9b6dcd801
NadineAzebaze <nadine-carel.azebaze-zewo@etu.univ-cotedazur.fr>
2022-01-03 16:54:36 +0100

    Robot saving position

--- fr.univcotedazur.kairos.webots.polycreate.controler/src/fr/univcotedazur/kairos/webots/polycreate/controler/PolyCreateControler.java
+++ fr.univcotedazur.kairos.webots.polycreate.controler/src/fr/univcotedazur/kairos/webots/polycreate/controler/PolyCreateControler.java
@@ -297,6 +297,7 @@ public class PolyCreateControler extends Supervisor {
 		stop();
 		closeGripper();
 		System.out.println("Gripper closed\n");
+		this.carryObject = true;
 	}
 	
 	public void turnRound() {
@@ -308,6 +309,12 @@ public class PolyCreateControler extends Supervisor {
 		this.turn(Math.PI);
 		flushIRReceiver();
 		this.theChecker.freePathCheck();
+	}
+	
+	public void checkGripping() {
+		if(this.carryObject) {
+			theCtrl.raiseTheObjectIsGrip();
+		}
 	}
 	
 	/**
